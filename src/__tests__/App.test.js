f511122c795a8c38ee3346e1a3276e98920c15fb
marysol <marilirulita@gmail.com>
2022-03-12 13:24:12 -0800

    add link test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,7 +1,9 @@
 import renderer from 'react-test-renderer';
+import { Link } from 'react-router-dom';
 import '@testing-library/jest-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { RiArrowLeftSLine } from 'react-icons/ri';
 import store from '../redux/configureStore';
 import App from '../App';
 
@@ -15,5 +17,15 @@ it('renders correctly', () => {
       </Router>,
     )
     .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('Link is rendered', () => {
+  const component = renderer.create(
+    <Router>
+    <Link to="/"><RiArrowLeftSLine size="35px" color="white" /></Link>
+    </Router>,
+  );
+  let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
